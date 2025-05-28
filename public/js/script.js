a475d2e45b0098881314a927d91f47119a91cddc
Dendy Putra Pratama <ouxvre@gmail.com.com>
2025-05-28 14:04:51 +0700

    p

--- public/js/script.js
+++ public/js/script.js
@@ -23,7 +23,7 @@ window.addEventListener("scroll", () => {
 const lines = [
   { id: "line1", text: "SELAMAT DATANG DI" },
   { id: "line2", text: "SMK NEGERI 1" },
-  { id: "line3", text: "BINTAN UTARA" }
+  { id: "line3", text: "BINTAN UTARA" },
 ];
 
 function typeSmooth(lineIndex = 0, charIndex = 0) {
@@ -37,10 +37,10 @@ function typeSmooth(lineIndex = 0, charIndex = 0) {
   if (charIndex < text.length) {
     const span = document.createElement("span");
     span.className = "typing-letter";
-    
+
     // Tambahkan spasi dengan non-breaking space
     span.innerHTML = text[charIndex] === " " ? "&nbsp;" : text[charIndex];
-    
+
     container.appendChild(span);
 
     // Delay kemunculan
@@ -55,3 +55,55 @@ function typeSmooth(lineIndex = 0, charIndex = 0) {
 window.addEventListener("DOMContentLoaded", () => {
   typeSmooth();
 });
+
+//slider wok
+let nextDom = document.getElementById("next");
+let prevDom = document.getElementById("prev");
+
+let carouselDom = document.querySelector(".carousel");
+let SliderDom = carouselDom.querySelector(".carousel .list");
+let thumbnailBorderDom = document.querySelector(".carousel .thumbnail");
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll(".item");
+let timeDom = document.querySelector(".carousel .time");
+
+thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+let timeRunning = 3000;
+let timeAutoNext = 7000;
+
+nextDom.onclick = function () {
+  showSlider("next");
+};
+
+prevDom.onclick = function () {
+  showSlider("prev");
+};
+let runTimeOut;
+let runNextAuto = setTimeout(() => {
+  next.click();
+}, timeAutoNext);
+function showSlider(type) {
+  let SliderItemsDom = SliderDom.querySelectorAll(".carousel .list .item");
+  let thumbnailItemsDom = document.querySelectorAll(
+    ".carousel .thumbnail .item"
+  );
+
+  if (type === "next") {
+    SliderDom.appendChild(SliderItemsDom[0]);
+    thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+    carouselDom.classList.add("next");
+  } else {
+    SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
+    thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
+    carouselDom.classList.add("prev");
+  }
+  clearTimeout(runTimeOut);
+  runTimeOut = setTimeout(() => {
+    carouselDom.classList.remove("next");
+    carouselDom.classList.remove("prev");
+  }, timeRunning);
+
+  clearTimeout(runNextAuto);
+  runNextAuto = setTimeout(() => {
+    next.click();
+  }, timeAutoNext);
+}
